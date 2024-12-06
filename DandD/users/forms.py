c65c805a272d0116a550403530ccce0439b9b925
Azik Temirbolat <programmingsince2023@gmail.com>
2024-12-06 23:38:10 +0500

    authentication and header, registration, login

--- DandD/users/forms.py
+++ DandD/users/forms.py
@@ -6,16 +6,24 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = (
-            'first_name',
-            'last_name',
             'username',
             'email',
             'password1',
             'password2',
         )
-    first_name = forms.CharField()
-    last_name = forms.CharField()
     username = forms.CharField()
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password"
+        )
+    
--- DandD/users/views.py
+++ DandD/users/views.py
@@ -1,35 +1,95 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from users.forms import UserRegistrationForm
+from django.template import context
+from users.forms import UserLoginForm, UserRegistrationForm
 from django.contrib import auth, sessions
 from django.urls import reverse
 
-def login(request):
-
-
-    return render(request, "users/login.html")
 
 def registration(request):
     
-    if request.method == "POST":
-        form = UserRegistrationForm(data = request.POST)
+    if request.method == 'POST':
+
+        form = UserRegistrationForm(data=request.POST)
+        print('post')
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
 
-            user = auth.authenticate(username = username, password = password)
-            # sessions_key = request.sessions.sessions_key
+            form.save()
+            user = form.instance
 
-            if user:
-                auth.login(request,user)
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:home'))
+    else:
+        print('nepost')
+        form=UserRegistrationForm()
 
-                return HttpResponseRedirect(reverse('main:home'))
-
-    # else:
-    #     form = UserLoginForm()
-        
     context = {
         'form': form,
     }
 
-    return render(request, "users/registration.html", context)
+    return render(request,'users/registration.html',context)
+
+def login(request):
+
+    if request.method == 'POST':
+
+        form = UserLoginForm(data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:home'))
+    else:
+        form = UserLoginForm()
+
+    context={
+        "form": form
+    }
+
+    return render(request, 'users/login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:home'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username = username, password=password)
+
+#             if user:
+#                 auth.login(request,user)
+
+#                 return HttpResponseRedirect(reverse('main:index'))
+#     else:
+#         form = UserLoginForm()
+    
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, "users/login.html", context)
+
